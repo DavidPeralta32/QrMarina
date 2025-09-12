1f8ca4de8664ca7f20e886ea9cb33fcd1c1cf03f
Juan Carlos Colorado Cordova <jcolorado@unindetec.edu.mx>
2025-09-12 11:23:01 -0600

    Comentario en el modelo de usuario.

--- app/src/main/java/com/unindetec/qrmarina/model/Usuario.kt
+++ app/src/main/java/com/unindetec/qrmarina/model/Usuario.kt
@@ -1,7 +1,7 @@
 package com.unindetec.qrmarina.model
 
 data class Usuario (
-    val grado: String,
+    val grado: String, //grado
     val nombre: String,
     val fechaVigencia: String,
     val placas: String
